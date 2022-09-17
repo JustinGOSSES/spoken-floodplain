5919836182047fe7fb33687aeaa66a04b6408379
Justin Gosses <11600445+JustinGOSSES@users.noreply.github.com>
2022-09-17 15:15:21 -0500

    eh

--- js/index.js
+++ js/index.js
@@ -32,28 +32,28 @@ let config = {
   // of course you can use jquery, axios etc.
   fetch("data/created_data/FEMA_FIRM_FloodPolygons_forDisplay_fromNFHL_exported2022_09.geojson", {
     headers : { 
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
+      // 'Content-Type': 'application/json',
+      // 'Accept': 'application/json'
      }
 
   })
     .then(function (response) {
-      console.log('Error parsing JSON from response:', response); 
+      console.log('response message:', response); 
       //var data = response.json()
       //console.log('data early, json :', data);
       // await addFeatureToMap(data) 
-      setTimeout(function() {
-        addFeatureToMap(response.json()) ;
-      }, 1000);
-      // return response.json();
+      // setTimeout(function(data) {
+      //   return addFeatureToMap(data) ;
+      // }, 10000);
+      return response.json();
     })
-    // .then(function (data) {
-    //   // use geoJSON
-    //   console.log('data', data); 
-    //   L.geoJSON(data, {
-    //     onEachFeature: onEachFeature,
-    //   }).addTo(map);
-    // });
+    .then(function (data) {
+      // use geoJSON
+      console.log('data', data); 
+      L.geoJSON(data, {
+        onEachFeature: onEachFeature,
+      }).addTo(map);
+    });
 
 function addFeatureToMap(data){
   console.log('data', data); 
@@ -63,32 +63,33 @@ function addFeatureToMap(data){
 }
 
 
-// function getLocation(x) {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(showPosition,x);
-//   } else {
-//     x.innerHTML = "Geolocation is not supported by this browser.";
-//   }
-// }
+function getLocation(x) {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition,x);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
 
-// function showPosition(position,x) {
-//   document.getElementById("location").innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-// }
+function showPosition(position,x) {
+  document.getElementById("location").innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
 
 
-// function showError(error) {
-//   switch(error.code) {
-//     case error.PERMISSION_DENIED:
-//       x.innerHTML = "User denied the request for Geolocation."
-//       break;
-//     case error.POSITION_UNAVAILABLE:
-//       x.innerHTML = "Location information is unavailable."
-//       break;
-//     case error.TIMEOUT:
-//       x.innerHTML = "The request to get user location timed out."
-//       break;
-//     case error.UNKNOWN_ERROR:
-//       x.innerHTML = "An unknown error occurred."
-//       break;
-//   }
-// }
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      break;
+  }
+}
+
