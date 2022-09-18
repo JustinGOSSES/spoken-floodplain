526448034b63fc03d36ca93d6d567095d55cce25
Justin Gosses <11600445+JustinGOSSES@users.noreply.github.com>
2022-09-17 21:04:18 -0500

    added some speaking parts and fixed bug in turf code where points instead of point was used

--- js/index.js
+++ js/index.js
@@ -5,7 +5,12 @@
 
 /////////////////////////////////
 
+
+//// An initial position point for the map set as a global variable. 
+//// It might be changed by other JavaScript.
 var dataGlobal = [-95.498,29.7604]
+var polygons = ""
+var isLocationWithinOneFloodplainPolygon = false
 
 // config map
 let config = {
@@ -15,8 +20,8 @@ let config = {
   // magnification with which the map will start
   const zoom = 10;
   // co-ordinates 29.7604, -95.498
-  const lat = 29.80;
-  const lng = -95.38;
+  const lat = dataGlobal[1]
+  const lng = dataGlobal[0] //-95.38;
   
   // calling map
   const map = L.map("map", config).setView([lat, lng], zoom);
@@ -53,12 +58,11 @@ let config = {
     })
     .then(function (data) {
       // use geoJSON
-      dataGlobal = data
+      polygons = data
       console.log('data', data); 
       L.geoJSON(data, {
         onEachFeature: onEachFeature,
       }).addTo(map);
-      searchWithinPolygonsForPoint(data)
     });
 
 function addFeatureToMap(data){
@@ -71,8 +75,9 @@ function addFeatureToMap(data){
 
 function getLocation(x) {
   if (navigator.geolocation) {
+    //// This calls the function showPosition with an argument of the position of the device.
+    //// We'll assume that the geojson polygons is already loaded.
     navigator.geolocation.getCurrentPosition(showPosition);
-
   } else {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
@@ -80,11 +85,19 @@ function getLocation(x) {
 
 
 function showPosition(position) {
+  //// This resets the center of the map based on the location position from the device.
   map.setView([position.coords.latitude, position.coords.longitude], 15)
-  turfPoint = turf.point([position.coords.longitude,position.coords.latitude]);
-  let working = new SpeechSynthesisUtterance("Your location is ."+Math.round(position.coords.latitude)+" latitude and"+Math.round(position.coords.longitude)+" longitudes"); 
-    window.speechSynthesis.speak(working);
+  //// Creates a turf point from the position location coordinates.
+  turfPoints = turf.points([[position.coords.longitude,position.coords.latitude]]);
+  //// This calls the text to speech capabilities of the browser and says the location
+  let currentLocationSpeak = new SpeechSynthesisUtterance("Your location is ."+Math.round(position.coords.latitude)+" latitude and"+Math.round(position.coords.longitude)+" longitudes"); 
+    window.speechSynthesis.speak(currentLocationSpeak);
+  //// This updates the position on the HTML page.
   document.getElementById("location").innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+  //// Add marker for location point onto map:
+  const marker1 = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+  //// Call function to see if location in polygons
+  searchWithinPolygonsForPoint(polygons,turfPoints)
 }
 
 function showError(error) {
@@ -106,32 +119,30 @@ function showError(error) {
 
 ////////// TURF CODE
 
-var turfPoint = turf.point([-95.498,29.7604]);
+//var turfPoint = turf.point([-95.498,29.7604]);
 
-function searchWithinPolygonsForPoint(Polygons){
-  console.log("turfPoint",turfPoint)
-  //turfPoint
-  //polygon
-  ////
-  //var pointsFP = turf.points([[-95.380911,29.815999]]);
+function searchWithinPolygonsForPoint(Polygons,turfPoints,isLocationWithinOneFloodplainPolygon){
+  console.log("turfPoint",turfPoints)
+  console.log("Polygons",Polygons)
   var dataGlobal = [-95.498,29.7604]
-  var pointsFP = turf.points([dataGlobal]);
-
   var temp_point_in_floodplain = []
   var numberPolygons = Polygons.features.length
   console.log("number of polygons",numberPolygons)
   console.log('Polygons.features 0',Polygons.features[0])
   for (let i = 0; i < numberPolygons; i++) {
-    //console.log("Polygons.features",Polygons.features[0].geometry.coordinates)
     var searchWithin = turf.polygon(Polygons.features[0].geometry.coordinates);
-    // var searchWithin = await turf.multiPolygon(Polygons.features.geometry.coordinates);
-    var ptsWithin = turf.pointsWithinPolygon(pointsFP, searchWithin);
+    var ptsWithin = turf.pointsWithinPolygon(turfPoints, searchWithin);
     if(ptsWithin.features.length != 0){
       console.log("location is within this polyon",ptsWithin);
+      //// This calls the text to speech capabilities of the browser and says a user is within the floodplain
     }
-    // console.log("ptsWithin",ptsWithin);
   }
-  // var searchWithin = turf.multiPolygon(Polygons.features.geometry.coordinates);
-  // var ptsWithin = turf.pointsWithinPolygon(pointsFP, searchWithin);
-  // console.log("ptsWithin",ptsWithin);
+  if (isLocationWithinOneFloodplainPolygon == true){
+    let withinFloodplainSpeak = new SpeechSynthesisUtterance("Your recently measured location is within the floodplain."); 
+    window.speechSynthesis.speak(withinFloodplainSpeak);
+  }
+  else{
+    let notWithinFloodplainSpeak = new SpeechSynthesisUtterance("Your recently measured location is outside the floodplain."); 
+    window.speechSynthesis.speak(notWithinFloodplainSpeak);
+  }
 }
